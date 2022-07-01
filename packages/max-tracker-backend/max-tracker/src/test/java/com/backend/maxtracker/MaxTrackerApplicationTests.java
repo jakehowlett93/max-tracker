f39fd52227abf5a428081492c145083cb3341438
Jake Howlett <jake.howlett93@gmail.com>
2022-07-01 12:53:44 +0100

    springboot initial commit

---
+++ packages/max-tracker-backend/max-tracker/src/test/java/com/backend/maxtracker/MaxTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.maxtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
