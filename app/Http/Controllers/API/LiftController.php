ee716dbf3356fc12ed42b1842bdd41b77a7ab1f3
Jake Howlett <jake.howlett93@gmail.com>
2022-07-20 13:30:09 +0100

    started creating lift controller

---
+++ app/Http/Controllers/API/LiftController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Lift;
+
+class LiftController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $lifts = Lift::all();
+
+        return response()->json($lifts, 201);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'exercise'=>'required',
+            'weight'=>'required',
+        ]);
+
+        $lift = new Lift([
+            'exercise' => $request->get('exercise'),
+            'weight' => $request->get('weight'),
+        ]);
+        $lift->save();
+
+        return response()->json($lift, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- app/Models/Lift.php
+++ app/Models/Lift.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Lift extends Model
 {
     use HasFactory;
+
+    protected $table = 'lifts';
 }
